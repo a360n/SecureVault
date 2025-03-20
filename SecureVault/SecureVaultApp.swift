37f448c8e3e2caf88a713c33f0e47e51a3e82e93
ALI <alinasserkhazali@gmail.com>
2025-03-20 17:08:43 +0300

    Initial Commit

---
+++ SecureVault/SecureVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureVaultApp.swift
+//  SecureVault
+//
+//  Created by Ali Al-Khazali on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SecureVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
